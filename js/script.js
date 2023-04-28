33b053c2f47060232c45042030c23f543e0d6289
Gabriel Medina <medina.rj@live.com>
2023-04-27 23:59:39 -0300

    Aula 5

--- js/script.js
+++ js/script.js
@@ -1,13 +1,33 @@
 async function buscaEndereco(cep) {
+    let msgErro = document.getElementById('erro');
+    msgErro.innerHTML = "";
     try{
         let consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         let consultaConvertida = await consultaCEP.json();
+        
         if(consultaConvertida.erro){
             throw Error('CEP inválido');
         }
-        console.log(consultaConvertida);
-        return consultaConvertida
+
+        preencheCampos(consultaConvertida);
+
     }catch(erro){
-        console.log(erro);
+        msgErro.innerHTML = `<p>Cep inválido. Tente novamente</p>`;
     }
 }
+
+function preencheCampos(dados) {
+    let endereco = dados.logradouro;
+    let bairro = dados.bairro;
+    let cidade = dados.localidade;
+    let uf = dados.uf;
+    
+    document.getElementById('endereco').value = endereco;
+    document.getElementById('bairro').value = bairro;
+    document.getElementById('cidade').value = cidade;
+    document.getElementById('estado').value = uf;
+
+}
+
+let campoCEP = document.getElementById('cep');
+campoCEP.addEventListener('focusout', () => buscaEndereco(campoCEP.value));
